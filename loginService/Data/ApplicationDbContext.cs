620afdf1ae183361c34a3ca92bfb9e44a38fa8ef
Thevin Malaka <thevin@orelit.com>
2023-07-20 23:21:26 +0530

    EF added

---
+++ loginService/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+//namespace loginService.Data
+//{
+//	public class ApplicationDbContext
+//	{
+//		public ApplicationDbContext()
+//		{
+//		}
+//	}
+//}
+
+
+
+using loginService;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+}
+
+
--- loginService/WeatherForecast.cs
+++ loginService/WeatherForecast.cs
@@ -1,13 +1,17 @@
-﻿namespace loginService;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace loginService;
 
 public class WeatherForecast
 {
-    public DateOnly Date { get; set; }
+    [Key]
+    public string Id { get; set; }
+
+    public DateTime Date { get; set; }
 
     public int TemperatureC { get; set; }
 
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-    public string? Summary { get; set; }
+    public string Summary { get; set; }
 }
-
