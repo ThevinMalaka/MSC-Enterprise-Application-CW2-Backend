8f9e7a44a0545b1d7bc3668aef6810536b7fd335
Thevin Malaka <thevin@orelit.com>
2023-07-21 20:21:07 +0530

    micro

--- courseworkApiGateway/Program.cs
+++ courseworkApiGateway/Program.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -33,24 +36,64 @@ using Ocelot.Middleware;
 
 // ---------------------
 
+//var builder = WebApplication.CreateBuilder(args);
+
+//builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+//builder.Services.AddOcelot(builder.Configuration);
+
+//var app = builder.Build();
+//app.MapGet("/", () => "Hello World");
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+//await app.UseOcelot();
+
+//app.Run();
+
+
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+// Add authentication services
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = "thevinmalaka.com",
+            ValidAudience = "thevinmalaka.com",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("ThisIsMySuperSecretKeyForFitnessAppInMyMSCourseWork"))
+        };
+    });
+
 builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
-app.MapGet("/", () => "Hello World");
+
+// Use authentication middleware
+app.UseAuthentication();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
 await app.UseOcelot();
 
 app.Run();
-
-
 
 
 
