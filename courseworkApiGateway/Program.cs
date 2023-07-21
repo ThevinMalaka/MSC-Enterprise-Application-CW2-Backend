4cf15f5bdfd64175ac932cbb9f5f9470cc788e32
Thevin Malaka <thevin@orelit.com>
2023-07-21 23:11:35 +0530

    user service

--- courseworkApiGateway/Program.cs
+++ courseworkApiGateway/Program.cs
@@ -59,6 +59,19 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+// Add CORS services.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowMyOrigin",
+    builder =>
+    {
+        builder.WithOrigins("http://localhost:3000") // React Web app running URL
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 // Add services to the container.
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
@@ -84,8 +97,9 @@ builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
-// Use authentication middleware
-app.UseAuthentication();
+app.UseCors("AllowMyOrigin"); // Use the CORS policy
+
+app.UseAuthentication(); // Use authentication middleware
 
 app.UseHttpsRedirection();
 
